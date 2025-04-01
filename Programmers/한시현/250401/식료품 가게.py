4562b1ee0af6a545125087a998b4b4b5f533a350
Shihyun Han <devowl99@gmail.com>
2025-04-01 18:00:35 +0900

    swea

--- Programmers/한시현/250401/식료품 가게.py
+++ Programmers/한시현/250401/식료품 가게.py
@@ -1,14 +1,13 @@
 T = int(input())
 for test_case in range(1, T + 1):
-    N = int(input()) # 품목 수
-    Pi = list(map(int, input().split())) # 정상가 & 할인가 2N개 (오름차순)
+    N = int(input())
+    Pi = list(map(int, input().split()))
 
     discount = []
     used = []
     for P in Pi:
-        if P != 0:
-            if int(P*(4/3)) in Pi:
-                discount.append(P)
+        if P != 0 and int(P*(4/3)) in Pi:
+            discount.append(P)
             Pi[Pi.index(int(P*(4/3)))] = 0
 
     print(f'#{test_case}', *discount)
