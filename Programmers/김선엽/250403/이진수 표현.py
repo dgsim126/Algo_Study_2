6ec08c9d2b573aea2eef3a1bf8365279f74b2f65
shipleaf <shipleaf@shipleafui-MacBookPro.local>
2025-04-04 20:11:22 +0900

    250404

--- Programmers/김선엽/250403/이진수 표현.py
+++ Programmers/김선엽/250403/이진수 표현.py
@@ -2,23 +2,23 @@ def solution(N, M):
     binary_ = bin(M)[2:]
     length = len(binary_)
 
-    print(binary_)
-
     if N > length:
         return False
     
     binary_ = list(binary_)[-N:]
 
-    print(binary_)
-
     if "1" not in binary_:
-        print(1)
         return False
     else:
         if len(set(binary_)) != 1:
-            print(2)
             return False
     
     return True
 
-print(solution(5, 62))
+T = int(input())
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())
+    if solution(N, M):
+        print(f"#{test_case} ON")
+    else:
+        print(f"#{test_case} OFF")
