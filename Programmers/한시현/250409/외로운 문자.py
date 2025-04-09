0ab41d4a77bbcb9c5d0fff60163aabff7cbadfba
Shihyun Han <devowl99@gmail.com>
2025-04-09 14:18:44 +0900

    swea

---
+++ Programmers/한시현/250409/외로운 문자.py
@@ -0,0 +1,16 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    words = input()
+    check_list = list(set(words))
+
+    answer = []
+    for word in check_list:
+        if words.count(word) % 2 != 0:
+            answer.append(word)
+    answer.sort()
+    str_answer = ''.join(answer)
+
+    if answer:
+        print(f'#{test_case} {str_answer}')
+    else:
+        print(f'#{test_case} Good')
