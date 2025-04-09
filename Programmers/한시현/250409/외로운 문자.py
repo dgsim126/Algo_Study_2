e6027ca1aebd994cdd1171dc103a03263c484a06
Shihyun Han <devowl99@gmail.com>
2025-04-09 14:29:07 +0900

    swea

--- Programmers/한시현/250409/외로운 문자.py
+++ Programmers/한시현/250409/외로운 문자.py
@@ -14,3 +14,24 @@ for test_case in range(1, T + 1):
         print(f'#{test_case} {str_answer}')
     else:
         print(f'#{test_case} Good')
+
+
+# Counter, dictionary 연습
+from collections import Counter
+
+T = int(input())
+for test_case in range(1, T + 1):
+    words = input()
+    word_counter = Counter(words)
+
+    answer = []
+    for k, v in word_counter.items():
+        if v % 2 != 0:
+            answer.append(k)
+    answer.sort()
+
+    str_answer = ''.join(answer)
+    if answer:
+        print(f'#{test_case} {str_answer}')
+    else:
+        print(f'#{test_case} Good')
